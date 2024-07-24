c86745e8f67dfc53c3b90ebb0e0d0c0f88511bbf
Ashish-smb <ashishkumar2004smb@gmail.com>
2024-07-25 01:19:30 +0530

    carousel-pending

--- assets/js/js.js
+++ assets/js/js.js
@@ -4,3 +4,21 @@ $('.hero-area-carousel').owlCarousel({
     autoplay:true,
     dots:true,
 });
+
+// tab menu
+const btn = $('.hair-deals .hairfall-stages ul > li');
+const tab = $('.hair-deals .hairfall-view > div ');
+btn.click(function() {
+    btn.removeClass('active');
+    $(this).addClass('active');
+    tab.removeClass('active');
+    $($(this).data('tab')).addClass('active');
+    console.log( $(this).data('tab') );
+});
+
+// product carousel 
+$('.product-carousel').owlCarousel({
+    items: 5,
+    loop:true,
+    margin:20,
+}); 
