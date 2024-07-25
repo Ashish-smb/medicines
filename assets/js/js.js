916fdf49660138f007d1c6f7b6394d7d5af49e3a
Ashish-smb <ashishkumar2004smb@gmail.com>
2024-07-25 09:05:55 -0700

    images

--- assets/js/js.js
+++ assets/js/js.js
@@ -17,8 +17,16 @@ btn.click(function() {
 });
 
 // product carousel 
-$('.product-carousel').owlCarousel({
+$('.deals-carousel').owlCarousel({
     items: 5,
     loop:true,
     margin:20,
 }); 
+// next prev carousel of deals carousel
+const guide = $('.deals-carousel');
+$('#deals-prev').click(function () {
+    guide.trigger('prev.owl.carousel');
+});
+$('#deals-next').click(function() {
+    guide.trigger('next.owl.carousel');
+});
